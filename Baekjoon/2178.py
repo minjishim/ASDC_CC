79cabc57002fe86a7f56fcf6d8bfe3ddc40f9791
JA <37624550+minjishim@users.noreply.github.com>
2022-10-01 13:25:14 +0000

    10주차 - 미로 탐색

---
+++ Baekjoon/2178.py
@@ -0,0 +1,45 @@
+# (10주차 : 그래프) 미로 탐색
+
+#첫째 줄에 두 정수 N, M(2 ≤ N, M ≤ 100)이 주어진다. 다음 N개의 줄에는 M개의 정수로 미로가 주어진다. 각각의 수들은 붙어서 입력으로 주어진다.
+
+from collections import deque
+
+# n, m을 공백으로 구분하여 입력받기
+n, m = map(int, input().split())
+
+# 2차원 리스트의 맵 정보 입력받기
+graph = []
+for i in range(n):
+  graph.append(list(map(int, input())))
+
+# 이동할 네 방향 정의(상, 하, 좌, 우)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+# BFS 소스코드 구현
+def bfs(x, y):
+  # 큐(Queue) 구현을 위해 deque 라이브러리 사용
+  queue = deque()
+  queue.append((x, y))
+  # 큐가 빌 때까지 반복
+  while queue:
+    x, y = queue.popleft()
+    # 현재 위치에서 네 방향으로의 위치 확인
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+      # 미로 찾기 공간을 벗어난 경우 무시
+      if nx < 0 or ny < 0 or nx >= n or ny >= m :
+        continue
+      # 벽인 경우 무시
+      if graph[nx][ny] == 0 :
+        continue
+      # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+      if graph[nx][ny] == 1:
+        # 해당 노드의 값을 1 증가하여 저장
+        graph[nx][ny] = graph[x][y] + 1
+        queue.append((nx, ny))
+  # 가장 오른쪽 아래까지의 최단 거리 반환
+  return graph[n-1][m-1]
+
+print((bfs(0,0)))
